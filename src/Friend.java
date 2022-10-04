a54f9aa0615b7af3184c8f6782b92e90d649fd05
LivNTI <liv.sundman@ga.ntig.se>
2022-10-04 13:57:49 +0200

    First

--- src/Friend.java
+++ src/Friend.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Friend {
+public class Friend {
+    //instance variables
+    private String name = "A friend";
+    private int hp = 100;
+
+    //Constructors
+    public Friend() {
+    }
+
+    public Friend(String name) {
+
+        this.name = name;
+    }
+
+    //Methods
+    public void hit(int damage) {
+        this.hp = this.hp - damage;
+    }
+
+
+    // Getters Setters
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner inLine = new Scanner(System.in);
+
+        Friend aFriend = new Friend();
+        Friend myFriend = new Friend("Filip");
+        System.out.println("my friend is called " + myFriend.getName());
+        System.out.println("What is your friends name: ");
+        String newName = inLine.next();
+
+        aFriend.setName(newName);
+        System.out.println("my old friend is now called " + aFriend.getName());
+        newName = inLine.next();
+        System.out.println();
     }
 }
